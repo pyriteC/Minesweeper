0028d16593935de6bae123877673a2d22d5a58a0
pyriteC <89157444+pyriteC@users.noreply.github.com>
2022-06-09 21:43:00 -0700

    IT WORKS

--- src/mine/view/Panel.java
+++ src/mine/view/Panel.java
@@ -1,7 +1,10 @@
 package mine.view;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -26,7 +29,7 @@ private JPanel buttonPanel;
 		setUpButtonGrid();
 		setUpButtonPanel();
 		setupPanel();
-//		setupListeners();
+		setupListeners();
 //		setupLayout();
 		
 	}
@@ -37,7 +40,8 @@ private JPanel buttonPanel;
 		{
 			for (int inIndex = 0; inIndex < buttonGrid[0].length; inIndex++)
 			{
-				JButton temp = new JButton(app.grid[index][inIndex].getAdjMineString());
+				JButton temp = new JButton();
+				temp.setSize(new Dimension(5000,5000));
 				buttonGrid[index][inIndex] = temp;
 			}
 		}
@@ -62,11 +66,103 @@ private JPanel buttonPanel;
 	
 	private void setupListeners()
 	{
-		
+		for ( int row = 0; row < buttonGrid.length; row ++)
+		{
+			final int rowIndex = row;
+			for ( int col = 0; col < buttonGrid[0].length; col++)
+			{
+				final int colIndex = col;
+				buttonGrid[row][col].addActionListener(click ->   onPress(rowIndex,colIndex));
+				buttonGrid[row][col].addMouseListener (new MouseAdapter()
+				{
+				    public void mouseClicked(MouseEvent e)
+				    {
+				         if (e.getButton() == 3 )//right click
+				         {
+				        	 flag(rowIndex,colIndex);
+				         }
+				        
+				    }
+				    
+				});
+			}
+		}
 	}
 	
 	private void setupLayout()
 	{
 	
+	}
+	
+	private void onPress(int row, int col)
+	{
+		if (app.grid[row][col].isMine())
+		{
+			gameOver();
+		}
+
+		buttonGrid[row][col].setText(app.grid[row][col].getAdjMineString());
+		buttonGrid[row][col].setEnabled(false);
+		if (app.grid[row][col].getAdjMine() == 0)
+		{
+			if (row > 0)//top
+			{
+				buttonGrid[row-1][col].setText(app.grid[row-1][col].getAdjMineString());
+				buttonGrid[row-1][col].setEnabled(false);
+			}
+			if ( row > 0 && col < app.getCols()-1 ) // top right 
+			{
+				buttonGrid[row-1][col+1].setText(app.grid[row-1][col+1].getAdjMineString());
+				buttonGrid[row-1][col+1].setEnabled(false);
+			}
+			if ( col < app.getCols() -1) //right
+			{
+				buttonGrid[row][col+1].setText(app.grid[row][col+1].getAdjMineString());
+				buttonGrid[row][col+1].setEnabled(false);
+			}
+			if ( col < app.getCols() -1 && row < app.getRows() -1)// bottom right 
+			{
+				buttonGrid[row+1][col+1].setText(app.grid[row+1][col+1].getAdjMineString());
+				buttonGrid[row+1][col+1].setEnabled(false);
+			}
+			if (row < app.getRows() -1)// bottom  
+			{
+				buttonGrid[row+1][col].setText(app.grid[row+1][col].getAdjMineString());
+				buttonGrid[row+1][col].setEnabled(false);
+			}
+			if ( col > 0 && row < app.getRows() -1)// bottom Left 
+			{
+				buttonGrid[row+1][col-1].setText(app.grid[row+1][col-1].getAdjMineString());
+				buttonGrid[row+1][col-1].setEnabled(false);
+			}
+			if ( col > 0)//  Left 
+			{
+				buttonGrid[row][col-1].setText(app.grid[row][col-1].getAdjMineString());
+				buttonGrid[row][col-1].setEnabled(false);
+			}
+			if ( col > 0 && row > 0)// top Left 
+			{
+				buttonGrid[row-1][col-1].setText(app.grid[row-1][col-1].getAdjMineString());
+				buttonGrid[row-1][col-1].setEnabled(false);
+			}
+		}
+	}
+
+	
+
+	private void flag(int row, int col) 
+	{
+		buttonGrid[row][col].setText("F");
+	}
+	
+	private void gameOver() 
+	{
+		for ( int row = 0; row < buttonGrid.length; row ++)
+		{
+			for ( int col = 0; col < buttonGrid[0].length; col++)
+			{
+				buttonGrid[row][col].setEnabled(false);
+			}
+		}
 	}
 }
